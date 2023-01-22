d62bdd89ad5406e7a3d4213e46b4df7210d455d8
thawhakyi <47990222+thawhakyi@users.noreply.github.com>
2023-01-23 03:21:15 +0630

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -29,9 +29,9 @@ module.exports = {
   // [OPTIONAL] We use Google Fonts natively for font customisations.
   // You can check and generate the required links and names at https://fonts.google.com.
   // googleFontSans - the sans serif font used in onedrive-vercel-index.
-  googleFontSans: 'Playfair Display', serif,
+  googleFontSans: 'Playfair Display',
   // googleFontMono - the monospace font used in onedrive-vercel-index.
-  googleFontMono: 'Space Mono', monospace,
+  googleFontMono: 'Space Mono',
   // googleFontLinks -  an array of links for referencing the google font assets.
   googleFontLinks: ['https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&family=Space+Mono&display=swap'],
 
